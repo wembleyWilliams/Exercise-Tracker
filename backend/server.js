5cce2bae5c8d2b21ae4623a5f1fb1b3e7a6e2856
WembleyWilliamsDev <wembleywilliams1998@gmail.com>
2021-01-13 15:38:01 -0500

    Added backend and prepared front end connections

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,17 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUriParser: true, useCreateIndex:true});
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true});
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection successfully established")
 })
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter)
+app.use('/users', usersRouter)
 
 //This starts the server listening on a certain port
 app.listen(port, () => {
