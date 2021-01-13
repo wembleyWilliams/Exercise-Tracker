607e67c1747e9d97b4066e300851a53dc8e8db2d
WembleyWilliamsDev <wembleywilliams1998@gmail.com>
2021-01-13 16:53:53 -0500

    Added all fully functional components

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class ExercisesList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.deleteExercise = this.deleteExercise.bind(this);
+
+        this.state = {exercises:[]};
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:5000/exercises/')
+        .then(response => {
+            this.setState({ exercises: response.data })
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    deleteExercise() {
+        
+    }
+
     render() {
         return(
             <div>
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -5,17 +5,21 @@ export default class Navbar extends Component {
 
     render(){
         return(
-            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+            <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
             <Link to="/" className="navbar-brand">Exercise Tracker</Link>
-            <div className="collapse navbar-collapse">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
-                    <li className="navbar-item">
+                    <li className="nav-item">
                         <Link to="/" className="nav-link">Exercises</Link>
                     </li>
-                    <li className="navbar-item">
+                    <li className="nav-item">
                         <Link to="/create" className="nav-link">Create Exercise Log</Link>
                     </li>
-                    <li className="navbar-item">
+                    <li className="nav-item">
                         <Link to="/user" className="nav-link">Create User</Link>
                     </li>
                 </ul>
