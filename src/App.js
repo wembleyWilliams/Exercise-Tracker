15e07520e1f6d12a11d0c8c1bb6c946838e30816
WembleyWilliamsDev <wembleywilliams1998@gmail.com>
2021-01-13 16:46:30 -0500

    Added create components for both exercise and user

--- src/App.js
+++ src/App.js
@@ -9,12 +9,6 @@ import EditExercise from "./components/edit-exercise.component";
 import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
 
-import Navbar from "./components/navbar.component";
-import ExercisesList from "./components/exercises-list.component";
-import EditExercise from "./components/edit-exercise.component";
-import CreateExercise from "./components/create-exercise.component";
-import CreateUser from "./components/create-user.component";
-
 function App() {
   return (
     <Router>
